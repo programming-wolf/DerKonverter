71ce33eed5c372ff9045a3baca1f66fb0ecc83e2
Ben Wolf <benjamin.wolf@innoq.com>
2018-11-23 16:55:12 +0100

    Fix bug pointed out by unit test

--- Converter/Converter.cs
+++ Converter/Converter.cs
@@ -8,8 +8,10 @@
             long result = 0;
             var i = 0;
             for (; i < 6; i++)
+            {
                 result = result << 8;
                 result |= array[i];
+            }
 
             return result;
         }
