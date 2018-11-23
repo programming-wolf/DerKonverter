027e7ce6273b9862cbb37cb88c184a2a12cfb571
Ben Wolf <benjamin.wolf@innoq.com>
2018-11-23 16:46:02 +0100

    Add default test class

---
+++ ConverterTest/ConverterTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ConverterTest
+{
+    [TestFixture]
+    public class ConverterTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
