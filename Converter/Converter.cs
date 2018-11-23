6b1c299b8c71eb7f1b961dd7d0a0d0f7b3afb191
Ben Wolf <benjamin.wolf@innoq.com>
2018-11-23 16:57:10 +0100

    Fix NullReferenceException

--- Converter/Converter.cs
+++ Converter/Converter.cs
@@ -1,16 +1,19 @@
 ﻿namespace Der.Konverter
 {
-
     public static class Converter
     {
         public static long ConvertByteArrayToLong(byte[] array)
         {
             long result = 0;
-            var i = 0;
-            for (; i < 6; i++)
+
+            if (array != null)
             {
-                result = result << 8;
-                result |= array[i];
+                var i = 0;
+                for (; i < 6; i++)
+                {
+                    result = result << 8;
+                    result |= array[i];
+                }
             }
 
             return result;
