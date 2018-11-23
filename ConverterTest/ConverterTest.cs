f6da37cc3de4e1a835db2ae512ea328a599c6014
Ben Wolf <benjamin.wolf@innoq.com>
2018-11-23 16:59:55 +0100

    Add test case for very large number

--- ConverterTest/ConverterTest.cs
+++ ConverterTest/ConverterTest.cs
@@ -7,6 +7,7 @@ namespace ConverterTest
     public class ConverterTest
     {
         [TestCase(2831, new byte[] {0, 0, 0, 0, 11, 15, 22, 17})]
+        [TestCase(288230376337249809, new byte[] {4, 0, 0, 0, 11, 15, 22, 17})]
         [TestCase(0, null)]
         public void ConvertByteArrayToLong(long number, byte[] numberAsByteArray)
         {
