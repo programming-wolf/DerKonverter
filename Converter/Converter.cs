8e79dab53cb19e271e328de7f0125810573ed25c
Ben Wolf <benjamin.wolf@innoq.com>
2018-11-23 10:17:00 +0100

    Add initial empty solution

---
+++ Converter/Converter.cs
@@ -0,0 +1,11 @@
+﻿namespace Der.Konverter
+{
+
+    public static class Converter
+    {
+        public static int ConvertToInt(byte[] aBytes)
+        {
+            return 0;
+        }
+    }
+}
