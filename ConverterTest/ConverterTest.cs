c1468055bda756e9ac2961b9eef509c9b69aa948
Ben Wolf <benjamin.wolf@innoq.com>
2018-11-23 17:01:06 +0100

    Fix apparently wrong test case

--- ConverterTest/ConverterTest.cs
+++ ConverterTest/ConverterTest.cs
@@ -6,7 +6,7 @@ namespace ConverterTest
     [TestFixture]
     public class ConverterTest
     {
-        [TestCase(2831, new byte[] {0, 0, 0, 0, 11, 15, 22, 17})]
+        [TestCase(185538065, new byte[] {0, 0, 0, 0, 11, 15, 22, 17})]
         [TestCase(288230376337249809, new byte[] {4, 0, 0, 0, 11, 15, 22, 17})]
         [TestCase(0, null)]
         public void ConvertByteArrayToLong(long number, byte[] numberAsByteArray)
